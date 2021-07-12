ddba3ea81dcc3b34c6f7bdce250381df93b2cb84
Rishabh Sharma <risharma18b@gmail.com>
2021-07-12 12:43:44 +0530

    Mockito new tests added

---
+++ src/test/java/com/springrest/springrest/NewMockTest.java
@@ -0,0 +1,49 @@
+package com.springrest.springrest;
+import java.util.*;
+import com.springrest.springrest.controller.MyController;
+import com.springrest.springrest.entities.Course;
+import com.springrest.springrest.services.CourseService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class NewMockTest {
+    @Mock
+    private CourseService helloService;
+
+    @InjectMocks
+    private MyController helloController;
+
+    @Before
+    public void play()
+    {
+        System.out.println("Before the test");
+    }
+    @Test
+    public void getCoursebyIdTest() {
+        long courseId = 1;
+        when(helloService.getCourse(courseId))
+                .thenReturn((new Course(1, "this is a new datasource string", "lol this works")));
+        assertEquals("this is a new datasource string", helloService.getCourse(courseId).getTitle());
+    }
+    @After
+    public void play1()
+    {
+        System.out.println("After the test");
+    }
+
+}
